dbee2c2feaae8f82d90110d57b28c680f3e46f1e
joao0310carlos@gmail.com <ds-mtec-3@servidor.acad>
2025-03-07 11:56:07 -0300

    @JoaoCarlosO

--- aula-02-projeto-the-games/controllers/routes.py
+++ aula-02-projeto-the-games/controllers/routes.py
@@ -3,6 +3,15 @@ from flask import render_template, request
 jogadores = ['iruh', 'davi_lambari', 'edsongf',
              'kioto', 'black butterfly', 'jujudopix']
 
+# Array de objetos
+gamelist = [{'Título': 'CS-GO',
+            'Ano': 2012,
+             'Categoria': 'FPS Online'}]
+
+consolelist = [{'Nome': 'PS5',
+                'Preço': 5.000,
+                'País': 'Brasil'}]
+
 
 def init_app(app):
     # Criando a rota principal do site
@@ -14,10 +23,8 @@ def init_app(app):
 
     @app.route('/games', methods=['GET', 'POST'])
     def games():
-        # Dicionário no Python(obejto)
-        game = {'Título': 'CS-GO',
-                'Ano': 2012,
-                'Categoria': 'FPS Online'}
+        # Acessando o primeiro jogo da lista de jogos
+        game = gamelist[0]
 
         if request.method == 'POST':
             if request.form.get('jogador'):  # name da input
@@ -27,3 +34,23 @@ def init_app(app):
                  'PUBG', 'Minecraft', 'Lost Ark', 'Counter-Strike: Global Offensive']
 
         return render_template('games.html', game=game, jogadores=jogadores, jogos=jogos)
+
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
+                gamelist.append({'Título': request.form.get('titulo'), 'Ano': request.form.get(
+                    'ano'), 'Categoria': request.form.get('categoria')})
+
+        return render_template('cadgames.html', gamelist=gamelist)
+    
+    @app.route('/consoles', methods=['GET', 'POST'])
+    def consoles():
+
+        if request.method == 'POST':
+            if request.form.get('nome') and request.form.get('preco') and request.form.get('pais'):
+                gamelist.append({'Nome': request.form.get('nome'), 'Preço': request.form.get(
+                    'preco'), 'País': request.form.get('pais')})
+
+        return render_template('consoles.html', consolelist=consolelist)
